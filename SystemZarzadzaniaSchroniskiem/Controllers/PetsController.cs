44422f873a503b93cf63aed47fead11590812f62
Aleksander Chomicz <xchomiczo@gmail.com>
2025-04-26 19:51:20 +0200

    dodanie widoku bazy zwierzat; usuniecie log.txt z gita

---
+++ SystemZarzadzaniaSchroniskiem/Controllers/PetsController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SystemZarzadzaniaSchroniskiem.Areas.Identity.Data;
+using SystemZarzadzaniaSchroniskiem.Models;
+
+namespace SystemZarzadzaniaSchroniskiem
+{
+    public class PetsController : Controller
+    {
+        private readonly SystemZarzadzaniaSchroniskiemDbContext _context;
+
+        public PetsController(SystemZarzadzaniaSchroniskiemDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Pets
+        public async Task<IActionResult> Index()
+        {
+            var systemZarzadzaniaSchroniskiemDbContext = _context.Pets.Include(p => p.Breed);
+            return View(await systemZarzadzaniaSchroniskiemDbContext.ToListAsync());
+        }
+
+        // GET: Pets/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pet = await _context.Pets
+                .Include(p => p.Breed)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (pet == null)
+            {
+                return NotFound();
+            }
+
+            return View(pet);
+        }
+
+        // GET: Pets/Create
+        public IActionResult Create()
+        {
+            ViewData["BreedId"] = new SelectList(_context.Breeds, "Id", "Id");
+            return View();
+        }
+
+        // POST: Pets/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Description,AdmissionDate,AdoptionStatus,BirthDate,Weight,Species,Gender,BreedId")] Pet pet)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(pet);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BreedId"] = new SelectList(_context.Breeds, "Id", "Id", pet.BreedId);
+            return View(pet);
+        }
+
+        // GET: Pets/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pet = await _context.Pets.FindAsync(id);
+            if (pet == null)
+            {
+                return NotFound();
+            }
+            ViewData["BreedId"] = new SelectList(_context.Breeds, "Id", "Id", pet.BreedId);
+            return View(pet);
+        }
+
+        // POST: Pets/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description,AdmissionDate,AdoptionStatus,BirthDate,Weight,Species,Gender,BreedId")] Pet pet)
+        {
+            if (id != pet.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(pet);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PetExists(pet.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BreedId"] = new SelectList(_context.Breeds, "Id", "Id", pet.BreedId);
+            return View(pet);
+        }
+
+        // GET: Pets/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pet = await _context.Pets
+                .Include(p => p.Breed)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (pet == null)
+            {
+                return NotFound();
+            }
+
+            return View(pet);
+        }
+
+        // POST: Pets/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var pet = await _context.Pets.FindAsync(id);
+            if (pet != null)
+            {
+                _context.Pets.Remove(pet);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PetExists(int id)
+        {
+            return _context.Pets.Any(e => e.Id == id);
+        }
+    }
+}
